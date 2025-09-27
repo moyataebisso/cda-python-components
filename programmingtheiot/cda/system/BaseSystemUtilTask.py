d7c9ad7e1d9f3b05a00cfe67a3056e895a805ee7
MoyataEbisso <mebisso12@gmail.com>
2025-09-27 16:52:35 -0500

    Lab Module 02 - CDA system performance monitoring implementation

--- programmingtheiot/cda/system/BaseSystemUtilTask.py
+++ programmingtheiot/cda/system/BaseSystemUtilTask.py
@@ -1,32 +1,15 @@
-#####
-# 
-# This class is part of the Programming the Internet of Things
-# project, and is available via the MIT License, which can be
-# found in the LICENSE file at the top level of this repository.
-# 
-# You may find it more helpful to your design to adjust the
-# functionality, constants and interfaces (if there are any)
-# provided within in order to meet the needs of your specific
-# Programming the Internet of Things project.
-# 
-
 import programmingtheiot.common.ConfigConst as ConfigConst
 
-class BaseSystemUtilTask():
-	"""
-	Shell implementation representation of class for student implementation.
-	
-	"""
-	
-	def __init__(self, name = ConfigConst.NOT_SET, typeID = ConfigConst.DEFAULT_SENSOR_TYPE):
-		pass
-	
-	def getName(self) -> str:
-		pass
-	
-	def getTypeID(self) -> int:
-		pass
-	
-	def getTelemetryValue(self) -> float:
-		pass
-	
+class BaseSystemUtilTask:
+    def __init__(self, name: str = ConfigConst.NOT_SET, typeID: int = ConfigConst.DEFAULT_SENSOR_TYPE):
+        self.name = name
+        self.typeID = typeID
+    
+    def getName(self) -> str:
+        return self.name
+    
+    def getTypeID(self) -> int:
+        return self.typeID
+    
+    def getTelemetryValue(self) -> float:
+        pass  # To be implemented by subclasses
